a343333566d9f1e0c674578fc1ec01ffb0e9a722
Dylan-Wyl10 <dylanwyl10@gmail.com>
2023-06-04 17:45:10 -0400

    0603update: YW, upload route generator/net

---
+++ src/sim_main.py
@@ -0,0 +1,20 @@
+"""
+Date: May 26, 2023
+Author: Yilin Wang
+Note: this script is the main script that simulates the CAV rerouting with TSC
+"""
+# Status check for Sumo environment
+import sys
+import os
+
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path.append(tools)
+else:
+    sys.exit("please declare environment variable 'SUMO_HOME'")
+
+import traci
+
+
+if __name__== '__main__':
+    traci.start(["sumo-gui", "-c", "sumo_cfg/toy_net/toy_test.sumocfg", "--lateral-resolution=0.1"])
---
+++ src/sumo_cfg/toy_net/od_generator/routegenerator.py
@@ -0,0 +1,12 @@
+# /usr/bin/env python
+"""
+Author: Yilin Wang
+Date: 06/01/2023
+"""
+
+import numpy as np
+import random
+
+
+def od_generator(filepath, seed):
+    random.seed()
