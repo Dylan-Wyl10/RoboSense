da20af1f2d49709dadd9956fa5a0f4b76961be84
Dylan-Wyl10 <dylanwyl10@gmail.com>
2023-06-07 18:10:10 -0400

    06/07/2023 update YW: add od trip, route file.

--- src/sumo_cfg/toy_net/od_generator/gen_od.py
+++ src/sumo_cfg/toy_net/od_generator/gen_od.py
@@ -7,12 +7,13 @@ Note: this script is to generate the od flow for toynet
 import argparse
 import numpy as np
 import json
-from routegenerator import od_generator
+from odgenerator import od_generator
+
 
 def main(args):
     time = args.timeframe  # length of the total simualtion
     path = args.savepath  # output path for route od file
-    od_generator(path, args.seed)
+    od_generator(path, args.timeframe, args.size, args.linkflow)
 
 
 if __name__ == '__main__':
@@ -23,14 +24,19 @@ if __name__ == '__main__':
                            help='simulation length (default:0)')
 
     argparser.add_argument('--savepath',
-                           default='tmp_save_route.rou.xml',
+                           default='od_file.od',
                            type=str,
                            help='save path for the route file')
 
-    argparser.add_argument('--seed',
-                           default=42,
+    argparser.add_argument('--size',
+                           default=6,
                            type=int,
-                           help='random seed')
+                           help='size for the network, must accomandate with taz file')
+
+    argparser.add_argument('--linkflow',
+                           default=100,
+                           type=int,
+                           help='hour rate for link flow rate')
 
     args = argparser.parse_args()
     main(args)
--- src/sumo_cfg/toy_net/od_generator/gen_od.sh
+++ src/sumo_cfg/toy_net/od_generator/gen_od.sh
@@ -1,1 +1,4 @@
-python3 gen_od.py --timeframe 3600 --savepath '../od_route.rou.xml'
+python3 gen_od.py --timeframe 3600 --savepath 'od_file.od' --size 6 --linkflow 300
+od2trips -n  taz_all.taz.xml -d od_file.od -o od_file.odtrips.xml
+cd ..
+duarouter -n toy_net1.net.xml --route-files od_generator/od_file.odtrips.xml -o toy_netOD.rou.xml
