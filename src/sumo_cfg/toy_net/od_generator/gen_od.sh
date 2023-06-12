0ecbb0015da7bea56882c98c415fd6e90a0db0aa
Dylan-Wyl10 <dylanwyl10@gmail.com>
2023-06-12 10:07:03 -0400

    111

--- src/sumo_cfg/toy_net/od_generator/gen_od.sh
+++ src/sumo_cfg/toy_net/od_generator/gen_od.sh
@@ -1,7 +1,8 @@
-python3 gen_od.py --timeframe 600 --savepath 'od_file.od' --size 6 --linkflow 300
+python3 gen_od.py --timeframe 600 --savepath 'od_file.od' --size 6 --linkflow 500
 od2trips -n  taz_all.taz.xml -d od_file.od -o od_file.odtrips.xml
 cd ..
 #duarouter -n toy_net1.net.xml --route-files od_generator/od_file.odtrips.xml -o toy_netOD.rou.xml
+rm -r "dualogs"
 mkdir "dualogs"
 cd dualogs
 python3 /usr/share/sumo/tools/assign/duaIterate.py -n ../toy_net1.net.xml -t ../od_generator/od_file.odtrips.xml --clean-alt
