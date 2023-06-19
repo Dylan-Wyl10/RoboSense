5794d821bbdb990cc6926ba944fa373f79976b10
Dylan-Wyl10 <dylanwyl10@gmail.com>
2023-06-19 00:25:59 -0400

    06/19/2023: finish data collection, update class

--- src/sim_main.py
+++ src/sim_main.py
@@ -7,6 +7,7 @@ Note: this script is the main script that simulates the CAV rerouting with TSC
 import sys
 import os
 import argparse
+from simulation import Simulation
 
 if 'SUMO_HOME' in os.environ:
     tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
@@ -22,8 +23,11 @@ import traci
 
 if __name__ == '__main__':
     # argparser = argparse.ArgumentParser(description=__doc__)
-    traci.start(["sumo-gui", "-c", "sumo_cfg/toy_net/toy_test.sumocfg", "--lateral-resolution=0.1", "--step-length=0.1"])
+    # traci.start(["sumo-gui", "-c", "sumo_cfg/toy_net/toy_test.sumocfg", "--lateral-resolution=0.1", "--step-length=0.1"])
 
+    path = "sumo_cfg/toy_net/toy_test.sumocfg"
+    s = Simulation(max_time=3600, link_num=60, resolution=0.1, config=path, time_interval=10)
+    s.sim()
 
     for i in range(36000):
         # print('yes')
