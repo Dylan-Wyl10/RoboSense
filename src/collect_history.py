5cb0a6580b7e229922bf01d78b2043c9ea8af12a
Dylan-Wyl10 <dylanwyl10@gmail.com>
2023-07-18 13:08:50 -0400

    update script combine flow and benchmark

--- src/collect_history.py
+++ src/collect_history.py
@@ -6,6 +6,8 @@ Note: this script is the script that used to collect historical travel informati
         2. Count the link flow
 """
 # Status check for Sumo environment
+import traci
+
 from simulation import Simulation
 import sys
 import os
@@ -26,15 +28,18 @@ if __name__ == '__main__':
     # argparser = argparse.ArgumentParser(description=__doc__)
     max_step = 36000  # define the maximum steps for the simulation
     path = "sumo_cfg/toy_net/toy_test_benchmark.sumocfg"
-    lf_table_savepath = "../result/link_flow/pr2_link_flow_3600.json"
-    # s = Simulation(max_time=3600, link_num=60, resolution=0.1, config=path, time_interval=10, lfHisPath=lf_table_path)
-    # s.sim()
+    lf_table_savepath = "../result/link_flow/link_flow_3600.json"
+    save_info = {'cover_table': "../result/PR2 Testing/cover_2000_step20.npy",
+                 'cover_table_benchmark': "../result/PR2 Testing/cover_table_benchmark1.npy"}
+
     s = Simulation(max_time=3600, link_num=60, resolution=0.1,
                    net_file='sumo_cfg/toy_net/toy_net1.net.xml',
                    time_interval=20)
     s.get_LF_table(config=path)
-    print('yesyes')
     s.save_lf(lf_table_savepath)
+    print('lf table has been saved')
+    traci.close()
+    s.sim_benchmark(save_info)
 
 
     # traci.start(["sumo-gui", "-c", "sumo_cfg/toy_net/toy_test.sumocfg", "--lateral-resolution=0.1", "--step-length=0.1"])
