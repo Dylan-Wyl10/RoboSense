205a5de69ae01c94d5a5a80ee392e659532698b6
Dylan-Wyl10 <dylanwyl10@gmail.com>
2023-09-29 10:55:05 -0400

    3

--- analysis/analysis4trip.py
+++ analysis/analysis4trip.py
@@ -10,6 +10,7 @@ import numpy as np
 import xml.dom.minidom
 
 def analysisTrip(trip_file):
+    print(trip_file)
 
 
 if __name__ == '__main__':
@@ -21,21 +22,8 @@ if __name__ == '__main__':
     file_dir['benchmark'] = "../result/PR{} TestingNew/sumolog_tmp/tripinfo_benchmark.xml".format(pr)
 
     for alp in alpha_set:
-        file_dir[str(alp)] = "../result/PR{} TestingNew/sumolog_tmp/tripinfor{}.xml".format(pr, alp)
+        file_dir[str(alp)] = "../result/PR{} TestingNew/sumolog_tmp/tripinfo{}.xml".format(pr, alp)
 
     print(file_dir)
-
-    #
-    # for p in p_set:
-    #     save_info = {'cover_table': "../result/PR{} TestingNew/pr{}_cover_{}_step{}.npy".format(pr, pr, p, step),
-    #                  'cover_table_benchmark': "../result/PR{} Testing/cover_table_benchmark.npy".format(pr)}
-    #
-    #     path = "sumo_cfg/toy_net/toy_test_{}.sumocfg".format(p)
-    #     lf_table_path = "../result/link_flow/pr{}_link_flow_3600.json".format(pr)
-    #     s = Simulation(max_time=3600, link_num=60, resolution=0.1,
-    #                    net_file='sumo_cfg/toy_net/toy_net1.net.xml',
-    #                    time_interval=step)
-    #     s.load_lf(lf_table_path)
-    #     s.sim(save_info, path, parameters=(1, p), deroute_num=2, k=256)
-    #     traci.close()
-    # # s.sim_benchmark(save_info)
+    for _, v in file_dir.items():
+        analysisTrip(v)
