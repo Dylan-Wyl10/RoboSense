631ef4c4f2de47828f8dae96b143a4832d728291
Dylan-Wyl10 <dylanwyl10@gmail.com>
2023-09-29 08:51:10 -0400

    111

--- analysis/analysis4trip.py
+++ analysis/analysis4trip.py
@@ -10,3 +10,25 @@ List:
 import numpy as np
 import xml.dom.minidom
 
+
+
+if __name__ == '__main__':
+
+    alpha_set = [0, 100, 300, 500, 1000, 2000]
+    pr = 5
+    step = 20
+    file_dir = {}
+
+    for p in p_set:
+        save_info = {'cover_table': "../result/PR{} TestingNew/pr{}_cover_{}_step{}.npy".format(pr, pr, p, step),
+                     'cover_table_benchmark': "../result/PR{} Testing/cover_table_benchmark.npy".format(pr)}
+
+        path = "sumo_cfg/toy_net/toy_test_{}.sumocfg".format(p)
+        lf_table_path = "../result/link_flow/pr{}_link_flow_3600.json".format(pr)
+        s = Simulation(max_time=3600, link_num=60, resolution=0.1,
+                       net_file='sumo_cfg/toy_net/toy_net1.net.xml',
+                       time_interval=step)
+        s.load_lf(lf_table_path)
+        s.sim(save_info, path, parameters=(1, p), deroute_num=2, k=256)
+        traci.close()
+    # s.sim_benchmark(save_info)
