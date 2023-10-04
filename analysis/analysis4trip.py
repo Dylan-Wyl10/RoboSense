42a557c681816902ae63a7994715b419da7caa83
Dylan-Wyl10 <dylanwyl10@gmail.com>
2023-10-04 17:27:22 -0400

    1

--- analysis/analysis4trip.py
+++ analysis/analysis4trip.py
@@ -18,17 +18,21 @@ if __name__ == '__main__':
     pr = 5
     step = 20
     file_dir = {}
+    file_dir['trip_bench'] = "../result/sumolog_pr{}/tripinfor_benchmark.xml".format(pr)
 
-    for p in p_set:
-        save_info = {'cover_table': "../result/PR{} TestingNew/pr{}_cover_{}_step{}.npy".format(pr, pr, p, step),
-                     'cover_table_benchmark': "../result/PR{} Testing/cover_table_benchmark.npy".format(pr)}
+    for a in alpha_set:
+        file_dir['trip_{}'.format(a)] = "../result/sumolog_pr{}/tripinfo{}.xml".format(pr, a)
 
-        path = "sumo_cfg/toy_net/toy_test_{}.sumocfg".format(p)
-        lf_table_path = "../result/link_flow/pr{}_link_flow_3600.json".format(pr)
-        s = Simulation(max_time=3600, link_num=60, resolution=0.1,
-                       net_file='sumo_cfg/toy_net/toy_net1.net.xml',
-                       time_interval=step)
-        s.load_lf(lf_table_path)
-        s.sim(save_info, path, parameters=(1, p), deroute_num=2, k=256)
-        traci.close()
-    # s.sim_benchmark(save_info)
+    
+    #     save_info = {'cover_table': "../result/PR{} TestingNew/pr{}_cover_{}_step{}.npy".format(pr, pr, a, step),
+    #                  'cover_table_benchmark': "../result/PR{} Testing/cover_table_benchmark.npy".format(pr)}
+    #
+    #     path = "sumo_cfg/toy_net/toy_test_{}.sumocfg".format(a)
+    #     lf_table_path = "../result/link_flow/pr{}_link_flow_3600.json".format(pr)
+    #     s = Simulation(max_time=3600, link_num=60, resolution=0.1,
+    #                    net_file='sumo_cfg/toy_net/toy_net1.net.xml',
+    #                    time_interval=step)
+    #     s.load_lf(lf_table_path)
+    #     s.sim(save_info, path, parameters=(1, p), deroute_num=2, k=256)
+    #     traci.close()
+    # # s.sim_benchmark(save_info)
