552081c5bfc8fdd00157597e31bc2910da004f48
Dylan-Wyl10 <dylanwyl10@gmail.com>
2023-11-07 01:20:59 -0500

    update flexibility routing logic, fix bugs

--- src/sim_main.py
+++ src/sim_main.py
@@ -27,8 +27,8 @@ if __name__ == '__main__':
     # traci.start(["sumo-gui", "-c", "sumo_cfg/toy_net/toy_test.sumocfg", "--lateral-resolution=0.1", "--step-length=0.1"])
 
     # p_set = [100, 300, 500, 1000, 2000]
-    p_set = [0, 100, 1000, 2000]
-    pr = 2
+    a_set = [1000]
+    # pr = 2  # penetration
     step = 20
 
     argparser = argparse.ArgumentParser(description=__doc__)
@@ -36,6 +36,10 @@ if __name__ == '__main__':
                            default='../sumo_cfg/5x5net',
                            type=str,
                            help='working dirctory for current simulation(sumo config)')
+    argparser.add_argument('--penetration',
+                           default=2,
+                           type=int,
+                           help='penetration rate in %')
     argparser.add_argument('--maxtime',
                            default=4200,
                            type=int,
@@ -47,18 +51,21 @@ if __name__ == '__main__':
                            help='network name used for configuration')
 
     args = argparser.parse_args()
+    pr = args.penetration
 
+    save_info = {'cover_table_benchmark': "../result/{}/PR{} Testing/cover_table_benchmark.npy".format(args.netname,
+                                                                                       pr)}
+    lf_table_path = "../result/{}/link_flow/pr{}_link_flow_3600.json".format(args.netname, pr)
 
-    for p in p_set:
-        save_info = {'cover_table': "../result/{}/PR{} TestingNew/pr{}_cover_{}_step{}.npy".format(args.netname, pr, pr, p, step),
-                     'cover_table_benchmark': "../result/PR{} Testing/cover_table_benchmark.npy".format(pr)}
+    for alpha in a_set:
+        save_info['cover_table{}'.format(alpha)] = "../result/{}/PR{} TestingNew/pr{}_cover_{}_step{}.npy".format(args.netname, pr, pr, alpha, step)
 
-        path = "../sumo_cfg/{}/case{}.sumocfg".format(args.netname, p)
-        lf_table_path = "../result/{}/link_flow/pr{}_link_flow_3600.json".format(args.netname, pr)
+        netpath = "../sumo_cfg/{}/case{}.sumocfg".format(args.netname, alpha)
+        # lf_table_path = "../result/{}/link_flow/pr{}_link_flow_3600.json".format(args.netname, pr)
         s = Simulation(start_time=600, max_time=args.maxtime, link_num=40, resolution=0.1,
                        net_file='../sumo_cfg/5x5net/5x5net.net.xml',
                        time_interval=20, sizeX=5, sizeY=5)
         s.load_lf(lf_table_path)
-        s.sim(save_info, path, parameters=(1, p), deroute_num=2, k=256)
+        s.sim(save_info, netpath, parameters=(1, alpha), flex=4, k=256)
         traci.close()
     # s.sim_benchmark(save_info)
