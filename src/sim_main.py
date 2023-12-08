4f3770ba4916b039dd10fe2cc75c17d8f15b91f9
Dylan-Wyl10 <dylanwyl10@gmail.com>
2023-12-08 08:33:24 -0500

    update 1208

--- src/sim_main.py
+++ src/sim_main.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     # traci.start(["sumo-gui", "-c", "sumo_cfg/toy_net/toy_test.sumocfg", "--lateral-resolution=0.1", "--step-length=0.1"])
 
     # p_set = [100, 300, 500, 1000, 2000]
-    a_set = [10, 0, 100]
+    a_set = [10, 0, 1]
     # pr = 2  # penetration
     step = 20
 
@@ -41,7 +41,7 @@ if __name__ == '__main__':
                            type=int,
                            help='penetration rate in %')
     argparser.add_argument('--maxtime',
-                           default=4200,
+                           default=2400,
                            type=int,
                            help='max simulation length(unit:s)')
 
