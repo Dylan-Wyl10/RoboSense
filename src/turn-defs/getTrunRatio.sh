341ca8832e54cc30a97f434ecd1ac4463d287c3a
Dylan-Wyl10 <dylanwyl10@gmail.com>
2024-02-28 20:43:58 -0500

    add turning ratio calculation

---
+++ src/turn-defs/getTrunRatio.sh
@@ -0,0 +1,6 @@
+read -p "provide the directory path for network configuration(from project root without/)": -e config_path
+read -p "provide the path for route file": -e route_file
+
+cd ../../$config_path
+
+python3 ../../src/turn-defs/generateTurnRatios.py -r $route_file -p
